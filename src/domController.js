4306e529ef4bd5027e60d8c5e3be7949cef724cc
jetfuzz <1jsf23@gmail.com>
2025-06-02 13:24:10 -0500

    Add project navigation and task delete functions
    - Add displayProjects and clearProjects functionality to domController
    - Add function to display current navigation tab
    - Add delete functionality to tasks

--- src/domController.js
+++ src/domController.js
@@ -1,6 +1,6 @@
-export { clearTasks, displayTasks, displayAllTasks };
-import editSvg from "./images/edit-3-svgrepo-com.svg";
-import deleteSvg from "./images/trash-svgrepo-com.svg";
+export { clearTasks, displayTasks, displayAllTasks, clearProjects, displayProjects };
+import editSvg from "./images/edit-2-svgrepo-com.svg";
+import deleteSvg from "./images/delete-svgrepo-com.svg";
 
 function clearTasks() {
     let contentDiv = document.getElementById("content");
@@ -14,6 +14,8 @@ function displayTasks(project) {
     project.tasks.forEach(task => {
         let taskDiv = document.createElement("div");
         taskDiv.className = "task";
+        taskDiv.dataset.taskId = task.id;
+        taskDiv.dataset.projectId = project.id;
 
         let taskCheckbox = document.createElement("div");
         taskCheckbox.className = "task-checkbox";
@@ -40,11 +42,9 @@ function displayTasks(project) {
         taskInfo.appendChild(taskDate);
         taskInfo.appendChild(taskEdit);
         taskInfo.appendChild(taskDelete);
-
         taskDiv.appendChild(taskCheckbox);
         taskDiv.appendChild(taskTitle);
         taskDiv.appendChild(taskInfo);
-
         contentDiv.appendChild(taskDiv);
     });
 }
@@ -52,5 +52,41 @@ function displayTasks(project) {
 function displayAllTasks(projects) {
     projects.forEach(project => {
         displayTasks(project);
+    });
+}
+
+function clearProjects() {
+    let nav = document.getElementById("nav");
+    let existingProjects = nav.querySelectorAll('.project-div');
+    existingProjects.forEach(project => project.remove())
+}
+
+function  displayProjects(projects) {
+    let nav = document.getElementById("nav");
+    projects.forEach(project => {
+        let projectDiv = document.createElement("div");
+        projectDiv.className = "project-div";
+        projectDiv.dataset.projectId = project.id;
+
+        let projectBtn = document.createElement("button");
+        projectBtn.className = "project-btn";
+        projectBtn.innerHTML = `# ${project.name}`
+
+        let projectInfo = document.createElement("div");
+        projectInfo.className = "task-info";
+
+        let projectEdit = document.createElement("img");
+        projectEdit.className = "task-edit";
+        projectEdit.src = editSvg;
+
+        let projectDelete = document.createElement("img");
+        projectDelete.className = "task-delete";
+        projectDelete.src = deleteSvg;
+
+        projectInfo.appendChild(projectEdit);
+        projectInfo.appendChild(projectDelete);
+        projectDiv.appendChild(projectBtn);
+        projectDiv.appendChild(projectInfo);
+        nav.appendChild(projectDiv);
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,101 @@
 import "./style.css";
 import * as todoManager from "./todoManager.js";
 import * as DOMController from "./domController.js"
-// import { addDays } from "date-fns";
+import { format, addDays } from "date-fns";
 
-let allBtn = document.getElementById("all-btn")
-
-
+//create default project
 let defaultProject = todoManager.addProject("Test Project");
+
+//placeholder projects + tasks for testing 
 let secondProject = todoManager.addProject("Second Project");
-todoManager.addTask(defaultProject, "Test Task", "Description", "2025-06-01", "high");
-todoManager.addTask(defaultProject, "Another Task", "Yes", "2025-06-01", "medium");
-todoManager.addTask(secondProject, "This Task", "Okay", "2025-06-02", "low")
+let thirdProject = todoManager.addProject("Third Project")
+todoManager.addTask(defaultProject, "Test Task", "Description", format(new Date(), "yyyy-MM-dd"), "high");
+todoManager.addTask(defaultProject, "Another Task", "Yes", format(new Date(), "yyyy-MM-dd"), "medium");
+todoManager.addTask(secondProject, "This Task", "Okay", format(addDays(new Date(), 2), "yyyy-MM-dd"), "low");
+todoManager.addTask(thirdProject, "Yet Another Task", "", format(addDays(new Date(), 8), "yyyy-MM-dd"), "medium");
 
-console.log(todoManager.projectArr);
+//display all tasks and projects on page load
+DOMController.displayAllTasks(todoManager.projectArr);
+DOMController.displayProjects(todoManager.projectArr);
+
+//dom elements
+let contentDiv = document.getElementById("content");
+let navDiv = document.getElementById("nav");
+
+let addTaskBtn = document.getElementById("add-task-btn");
+let allBtn = document.getElementById("all-btn");
+let todayBtn = document.getElementById("today-btn");
+let weekBtn = document.getElementById("week-btn");
 
 
+//track last clicked navigation tab when user edits/deletes a task
+let currentView = "all";
+let currentProject = null;
+
+//display the current view/project after task edit
+function displayCurrentView() {
+    if (currentProject === null) {
+        if (currentView === "all") {
+            DOMController.displayAllTasks(todoManager.projectArr);
+        } else if (currentView === "today") {
+            //display tasks due today
+        } else if (currentView === "week") {
+            //display tasks due this week
+        }
+    } 
+    else if (currentProject != null) {
+        let project = todoManager.projectArr.find((project) => project.id === currentProject);
+        DOMController.displayTasks(project)
+    } 
+}
+
+
+//nav events
+//add task
+
+//all tasks
 allBtn.addEventListener("click", () => {
     DOMController.clearTasks();
     DOMController.displayAllTasks(todoManager.projectArr);
+    currentView = "all";
+    currentProject = null;
+})
+
+
+//project events
+//switch project displayed
+navDiv.addEventListener("click", (e) => {
+    let projectDiv = e.target.closest(".project-div");
+    if (projectDiv) {
+        let projectId = parseInt(projectDiv.dataset.projectId);
+        currentProject = projectId;
+        let project = todoManager.projectArr.find((project) => project.id === projectId);
+        DOMController.clearTasks();
+        DOMController.displayTasks(project);
+    }
+})
+
+//add project
+
+
+//edit project
+
+
+//delete project
+
+
+//task events
+//edit
+
+//delete
+contentDiv.addEventListener("click", (e) => {
+    if(e.target.className === "task-delete") {
+        let taskDiv = e.target.closest('.task');
+        let taskId = parseInt(taskDiv.dataset.taskId);
+        let projectId = parseInt(taskDiv.dataset.projectId);
+        let project = todoManager.projectArr.find((project) => project.id === projectId);
+        todoManager.deleteTask(project, taskId);
+        DOMController.clearTasks();
+        displayCurrentView();
+    }
 })
